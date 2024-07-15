52dc983c814546f72adfb5b9d19c3fa79755e087
Conor Walsh <conoranderwalsh@gmail.com>
2024-07-16 00:21:36 +0200

    Fixed Anagram Groups

--- arrays_and_hashing/anagram_groups.cpp
+++ arrays_and_hashing/anagram_groups.cpp
@@ -1,50 +1,27 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> copy = strs;
+        unordered_map<string, vector<string>> map;
         vector<vector<string>> solution;
-        while (copy.begin() != copy.end()){
-            vector<string> group;
-            vector<string>::iterator start = copy.begin();
-            group.push_back(*start);
-            for (vector<string>::iterator next = start + 1; next != copy.end(); next++) {
-                if (isAnagram(*start, *next)) {
-                    group.push_back(*next);
-                    copy.erase(next);
-                }
-            }
-            copy.erase(start);
-            solution.push_back(group);
+        
+        for (string s : strs) {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            map[sorted].push_back(s);
         }
+
+        unordered_map<string, vector<string>>::iterator it = map.begin();
+        while (it != map.end()) {
+            solution.push_back(it->second);
+            it++;
+        }
+
         return solution;
     }
-
-    bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
-            return false;
-
-        unordered_map<char, int> s_set, t_set;
-        string::iterator s_it = s.begin(), t_it = t.begin();
-
-        while (s_it != s.end()) {
-            s_set[*s_it]++;
-            t_set[*t_it]++;
-            s_it++;
-            t_it++;
-        }
-        return s_set == t_set;
-    }
 };
-
-int main() {
-    Solution solution;
-    vector<string> anagrams = {"act","pots","tops","cat","stop","hat"};
-    vector<vector<string>> out;
-    out = solution.groupAnagrams(anagrams);
-    return 0;
-}
